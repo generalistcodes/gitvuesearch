8c465f1edc766e58ba1f1b5b70eb4ef631215a4e
kedomonzter <khemchay@gmail.com>
2020-10-08 04:22:30 +0200

    test(component): add test

--- src/test/GitSearch.test.ts
+++ src/test/GitSearch.test.ts
@@ -1,10 +1,25 @@
 import 'jest';
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import GitSearch from '@/components/GitSearch.vue';  
 
-describe('GitSearch', () => {
-    test('checking...', () => {
-        
-        console.log('GitSearch:', GitSearch)
-    });
+describe('InputSearch', () => {
+
+    test("Pass query value to the input", () =>{
+        const query = 'Angular';
+        const wrapper = shallowMount(GitSearch, {
+          propsData: { query },
+        });
+        expect(wrapper.text()).toMatch(query);
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    
+    // test("Pass query value to the input", () =>{
+    //     const wrapper = mount(GitSearch, {
+    //         propsData: {
+    //             query: 'angular'
+    //         }
+    //     })
+    //     expect(wrapper.html()).toMatchSnapshot()
+    // })
 });
